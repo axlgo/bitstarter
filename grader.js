323393225a365a7d73ef5398eaf562ea5faf63d9
Alex G <alexg@wendcentral.com>
2013-07-17 20:40:31 +0000

    updated version of grader to handle url option

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,19 +32,19 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); /* http://nodejs.org/api/process.html#process_process_exit_code */
     }
     return instr;
 };
-
+/*
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
-
+*/
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
-
+/*
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -54,21 +55,66 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
-
+*/
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
+    /* Workaround for commander.js issue.
+     * http://stackoverflow.com/a/6772648*/
     return fn.bind({});
+};
+
+/* return a synchronous function readFileSync() so we don't need
+ * to way for its response and not to worry about callbacks to
+ * handle it.
+ */
+function getBufferFromFile(file) {
+   return fs.readFileSync(program.file);
+};
+
+/* Hacky way to control asyncronous calls, we pass the
+ * scrappingJson function as a callback so after the
+ * buffer is fill from the url, the scrapping callback
+ * will be executed.
+ */
+function getBufferFromUrl(url, checks, callback) {
+    rest.get(url).on('complete', function(result) {
+       if(result instanceof Error) {
+          console.log("ERROR: Couldn't load the url requested!\n");
+          process.exit(1);
+       }
+       callback(checks,result);
+    });
+};
+
+/* scrappingJson function will need the checks data to parso on the
+ * buffer. Buffer is an abstracted from either a html file or from
+ * a url webpage. On each case there is a routine that will be returning
+ * buffer data so scrappingJson can use it.
+ */
+function scrappingJson(checks, buffer) {
+    $ = cheerio.load(buffer);
+    var scrapJson = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        scrapJson[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(scrapJson, null, 4);
+    console.log(outJson);
 };
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'Path to url link')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checksData = loadChecks(program.checks).sort();
+    if(!program.url) {
+       scrappingJson(checksData, getBufferFromFile(program.file));
+    } else {
+       getBufferFromUrl(program.url,checksData,scrappingJson);
+    }
+
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.scrappingJson = scrappingJson;
 }
